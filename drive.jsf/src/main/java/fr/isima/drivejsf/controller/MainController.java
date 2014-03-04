2786e622f76cc85c323735452f877bb61f1f9a01
Kévin Renella <kevin.renella@gmail.com>
2014-03-04 01:23:10 +0100

    File download on ContextMenu

--- drive.jsf/src/main/java/fr/isima/drivejsf/controller/MainController.java
+++ drive.jsf/src/main/java/fr/isima/drivejsf/controller/MainController.java
@@ -1,17 +1,21 @@
 package fr.isima.drivejsf.controller;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 
-import fr.isima.drivejsf.dao.DocumentDAO;
 import fr.isima.drivejsf.ejb.ServiceEJB;
+import fr.isima.drivejsf.entity.Data;
 import fr.isima.drivejsf.entity.Document;
-import fr.isima.drivejsf.entity.User;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 @ManagedBean
 @SessionScoped
@@ -20,6 +24,8 @@ public class MainController implements Serializable {
 	private static final long serialVersionUID = 3973801993975443027L;
 
     private List<Document> rootDocuments;
+    private Document selectedDocument = null;
+    private StreamedContent downloadableDocument = null;
 
 	@EJB
 	private ServiceEJB service;
@@ -45,5 +51,34 @@ public class MainController implements Serializable {
     public void setRootDocuments(List<Document> rootDocuments) {
         this.rootDocuments = rootDocuments;
     }
-	
+
+    public Document getSelectedDocument() {
+        return selectedDocument;
+    }
+
+    public void setSelectedDocument(Document selectedDocument) {
+        this.selectedDocument = selectedDocument;
+    }
+
+    public StreamedContent getDownloadableDocument() {
+        Document current = selectedDocument;
+        Data data;
+        InputStream stream;
+
+        if (current != null) {
+            data = current.getDataid();
+            stream = new ByteArrayInputStream(data.getData());
+            downloadableDocument = new DefaultStreamedContent(stream, "text/plain", current.getName());
+        } else {
+            downloadableDocument = null;
+        }
+        System.out.println(downloadableDocument);
+
+        return downloadableDocument;
+    }
+
+    public void setDownloadableDocument(StreamedContent downloadableDocument) {
+        this.downloadableDocument = downloadableDocument;
+    }
+
 }
